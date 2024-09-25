e8ced2b06244eb6e88da31eeac9ab9337e9f0d23
WhitZ <gxcarlos199@gmail.com>
2024-09-25 13:13:54 +0200

    sol

--- js/funcions.js
+++ js/funcions.js
@@ -1,3 +1,14 @@
-function confirmaRegistre(){
-    //completa
+function confirmaRegistre() {
+    // 2. Mostrar una alerta
+    alert("Registrant estudiant");
+
+    // 3. Mostrar un missatge a la consola
+    console.log("Registrant estudiant");
+
+    // 4. Modificar el contingut del div per mostrar el paràgraf
+    const div = document.getElementById("formulari");  // Assegura't que el formulari està dins un div amb id="formulari"
+    div.innerHTML = '<p class="important">T’has registrat amb èxit</p>';
+
+    // 6. Retornar false per evitar la recàrrega de la pàgina
+    return false;s
 }
